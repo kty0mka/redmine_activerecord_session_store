f246d9a08f35728d44e526fdc5724b1e2d9b5382
kty0mka <57333179+kty0mka@users.noreply.github.com>
2022-08-29 16:32:12 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,11 +1,8 @@
 Redmine::Plugin.register :redmine_activerecord_session_store do
   name 'Redmine ActiveRecord Session Store plugin'
-  author 'Nils Caspar'
+  author 'Nils Caspar / Artem Kalaidov'
   description 'This plugin teaches Redmine to use ActiveRecord as session store.'
-  version '0.0.1'
-  url 'https://github.com/pencil/redmine_activerecord_session_store'
-  author_url 'http://nilscaspar.ch/'
-
+  version '0.0.2'
+  url 'https://github.com/kty0mka/redmine_activerecord_session_store'
   RedmineApp::Application.config.session_store :active_record_store, :key => '_redmine_session'
 end
-Rails.logger.class.include ActiveSupport::LoggerSilence
