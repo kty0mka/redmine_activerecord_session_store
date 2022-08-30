ef4f6e223f4967f916196ab39b7d43408c91cd22
kty0mka <57333179+kty0mka@users.noreply.github.com>
2022-08-30 08:36:47 +0300

    Delete config directory

--- config/initializers/additional_environment.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.before_initialize do
-  logger            = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter  = config.log_formatter
-  config.logger     = ActiveSupport::TaggedLogging.new(logger)
-end
